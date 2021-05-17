cdb0832ca1b1f050bf78c4a4cd319e287502d0da
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2021-05-17 00:15:49 -0500

    less code

--- src/tools/sql_for_autocompletition.rs
+++ src/tools/sql_for_autocompletition.rs
@@ -1,48 +1,24 @@
-use sqlite::{self, Value};
+use rusqlite::{params, Connection, Result};
 
-use std::time::SystemTime;
 // select the last command use it with a resemblance
-pub fn select_commands(val: String) {
-    let sql ="SELECT command FROM commands WHERE command like ?1 ORDER BY last_time_used DESC --LIMIT 1;";
-    let conn = sqlite::open("src/database/database.db").unwrap();
-    let mut stm = conn.prepare(sql).unwrap().into_cursor();
-    // https://www.w3schools.com/sql/sql_wildcards.asp
-    // the like operator its like regex
-
-    stm.bind(&[Value::String(val + "%")]).unwrap();
-    while let Some(row) = stm.next().unwrap() {
-        println!("{:?}", row[0].as_string());
-    }
-}
-// update the  last time use it
 pub fn update_commands(val: String) {
-    println!("{:?}", val);
-    // esto no lo deberia de hacer ni por aaccidente pero no se por que no funciona
+    let conn = Connection::open("src/database/database.db").unwrap();
     let sql = "UPDATE commands SET last_time_used=strftime('%f', 'now')+ strftime('%s', 'now') WHERE command=?1;";
-    let conn = sqlite::open("src/database/database.db").unwrap();
-    let stm = conn.prepare(sql).unwrap();
-
-    stm.into_cursor().bind(&[Value::String(val)]).unwrap();
+    conn.execute(sql, params![val]).unwrap();
 }
-
-// insert the command to the database
 pub fn insert_command(val: String) {
-    let sql ="INSERT INTO commands(command,last_time_used) SELECT ?1,strftime('%f', 'now')+ strftime('%s', 'now')  WHERE NOT EXISTS(SELECT 1 FROM commands WHERE  command = ?1);";
-    let conn = sqlite::open("src/database/database.db").unwrap();
-    let mut stm = conn.prepare(sql).unwrap().into_cursor();
+    let sql = "UPDATE commands SET last_time_used=strftime('%f', 'now')+ strftime('%s', 'now') WHERE command=?1;";
+    let conn = Connection::open("src/database/database.db").unwrap();
+    conn.execute(sql, params![val]).unwrap();
+}
+pub fn select_commands(val: String) -> String {
+    let conn = Connection::open("src/database/database.db").unwrap();
+    let sql = "SELECT command FROM commands WHERE command like ?1 ORDER BY last_time_used DESC  1;";
 
-    stm.bind(&[
-        Value::String(val),
-        Value::Integer(
-            SystemTime::now()
-                .duration_since(SystemTime::UNIX_EPOCH)
-                .unwrap()
-                .as_secs() as i64, // im using this for get the time where the coommand has been use it
-        ),
-    ])
-    .unwrap();
-    // this look like shit but its necessary
-    // for update the row because we are using select , and for some reason it
-    // didnt function without this
-    while let Some(_) = stm.next().unwrap() {}
+    let mut rows = conn.prepare(sql).unwrap().query(params![val+"%"]).unwrap();
+    let mut command_output = String::new();
+    while let Some(row) = rows.next().unwrap() {
+        command_output = row.get(0).unwrap()
+    }
+    return command_output;
 }
