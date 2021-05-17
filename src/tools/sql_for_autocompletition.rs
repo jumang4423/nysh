c6224eb4ad6f1eac355aa9d67c9e73af545093f1
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2021-05-16 23:16:56 -0500

    less code

--- src/tools/sql_for_autocompletition.rs
+++ src/tools/sql_for_autocompletition.rs
@@ -2,23 +2,21 @@ use sqlite::{self, Value};
 use std::io;
 use std::time::SystemTime;
 // select the last command use it with a resemblance
-fn select_commands(val: String) {
+pub fn select_commands(val: String) {
+    let sql ="SELECT command FROM commands WHERE command like ?1 ORDER BY last_time_used DESC --LIMIT 1;";
     let conn = sqlite::open("src/database/database.db").unwrap();
-    let mut stm = conn
-        .prepare(
-            "SELECT * FROM commands WHERE command like ?1 ORDER BY last_time_used DESC LIMIT 1;",
-        )
-        .unwrap()
-        .into_cursor();
-    // https://www.w3schools.com/sql/sql_wildcards.asp 
+    let mut stm = conn.prepare(sql).unwrap().into_cursor();
+    // https://www.w3schools.com/sql/sql_wildcards.asp
     // the like operator its like regex
+
     stm.bind(&[Value::String(val + &"%")]).unwrap();
     while let Some(row) = stm.next().unwrap() {
-        println!("{:?}", row);
+        println!("{:?}", row[0].as_string());
     }
 }
 // update the  last time use it
-fn update_commands(val: String) {
+pub fn update_commands(val: String) {
+    let sql = "UPDATE commands SET last_time_used=?1 WHERE command=?2;";
     let values_insert = &[
         Value::Integer(
             SystemTime::now()
@@ -29,18 +27,15 @@ fn update_commands(val: String) {
         Value::String(val + "%"),
     ];
     let conn = sqlite::open("src/database/database.db").unwrap();
-    conn.prepare("UPDATE commands SET last_time_used=?1 WHERE command=?2;")
-        .unwrap()
-        .into_cursor()
-        .bind(values_insert)
-        .unwrap();
+    let mut stm = conn.prepare(sql).unwrap().into_cursor();
+    stm.bind(values_insert).unwrap();
+    while let Some(_) = stm.next().unwrap() {}
 }
-fn insert_command(val: String) {
+// insert the command to the database
+pub fn insert_command(val: String) {
+    let sql ="INSERT INTO commands(command,last_time_used) SELECT ?1,?2  WHERE NOT EXISTS(SELECT 1 FROM commands WHERE  command = ?1);";
     let conn = sqlite::open("src/database/database.db").unwrap();
-    let mut stm = conn
-        .prepare("INSERT INTO commands(command,last_time_used) SELECT ?1,?2  WHERE NOT EXISTS(SELECT 1 FROM commands WHERE  command = ?1);")
-        .unwrap()
-        .into_cursor();
+    let mut stm = conn.prepare(sql).unwrap().into_cursor();
 
     stm.bind(&[
         Value::String(val),
@@ -48,7 +43,7 @@ fn insert_command(val: String) {
             SystemTime::now()
                 .duration_since(SystemTime::UNIX_EPOCH)
                 .unwrap()
-                .as_secs() as i64,// im using this for get the time where the coommand has been use it
+                .as_secs() as i64, // im using this for get the time where the coommand has been use it
         ),
     ])
     .unwrap();
