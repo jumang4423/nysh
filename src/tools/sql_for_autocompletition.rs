12220121cc42274d7ee97859dcb1e16b83409e69
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2021-05-16 23:45:27 -0500

    Update sql_for_autocompletition.rs

--- src/tools/sql_for_autocompletition.rs
+++ src/tools/sql_for_autocompletition.rs
@@ -1,5 +1,5 @@
 use sqlite::{self, Value};
-use std::io;
+
 use std::time::SystemTime;
 // select the last command use it with a resemblance
 pub fn select_commands(val: String) {
@@ -9,31 +9,25 @@ pub fn select_commands(val: String) {
     // https://www.w3schools.com/sql/sql_wildcards.asp
     // the like operator its like regex
 
-    stm.bind(&[Value::String(val + &"%")]).unwrap();
+    stm.bind(&[Value::String(val + "%")]).unwrap();
     while let Some(row) = stm.next().unwrap() {
         println!("{:?}", row[0].as_string());
     }
 }
 // update the  last time use it
 pub fn update_commands(val: String) {
-    let sql = "UPDATE commands SET last_time_used=?1 WHERE command=?2;";
-    let values_insert = &[
-        Value::Integer(
-            SystemTime::now()
-                .duration_since(SystemTime::UNIX_EPOCH)
-                .unwrap()
-                .as_secs() as i64,
-        ),
-        Value::String(val + "%"),
-    ];
+    println!("{:?}", val);
+    // esto no lo deberia de hacer ni por aaccidente pero no se por que no funciona
+    let sql = "UPDATE commands SET last_time_used=strftime('%f', 'now')+ strftime('%s', 'now') WHERE command=?1;";
     let conn = sqlite::open("src/database/database.db").unwrap();
-    let mut stm = conn.prepare(sql).unwrap().into_cursor();
-    stm.bind(values_insert).unwrap();
-    while let Some(_) = stm.next().unwrap() {}
+    let stm = conn.prepare(sql).unwrap();
+
+    stm.into_cursor().bind(&[Value::String(val)]).unwrap();
 }
+
 // insert the command to the database
 pub fn insert_command(val: String) {
-    let sql ="INSERT INTO commands(command,last_time_used) SELECT ?1,?2  WHERE NOT EXISTS(SELECT 1 FROM commands WHERE  command = ?1);";
+    let sql ="INSERT INTO commands(command,last_time_used) SELECT ?1,strftime('%f', 'now')+ strftime('%s', 'now')  WHERE NOT EXISTS(SELECT 1 FROM commands WHERE  command = ?1);";
     let conn = sqlite::open("src/database/database.db").unwrap();
     let mut stm = conn.prepare(sql).unwrap().into_cursor();
 
