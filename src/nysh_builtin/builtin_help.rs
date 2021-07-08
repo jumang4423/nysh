6a88c834775c5180b0c7587749939975b791d4d0
jumang4423 <catontheskate4423@gmail.com>
2021-07-08 12:04:28 +0900

    now v0.1.0

--- src/nysh_builtin/builtin_help.rs
+++ src/nysh_builtin/builtin_help.rs
@@ -9,6 +9,7 @@ pub fn builtin_help() -> Result<(), String> {
     println!("-! {}: ls -al","la".magenta());
     println!("-! {}: cd ..","..".magenta());
     println!("-! {}: make a secret contents locker", "dream95".magenta());
+    println!("-! {}: displays given image on terminal directly", "nywer".magenta());
     println!("-! {}: help nysh","help".magenta());
     println!("-! {}: exit nysh","exit".magenta());
 
